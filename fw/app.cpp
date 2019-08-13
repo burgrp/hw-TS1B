298b692d37764e542320a2b5f179a510ff04c4cf
Pavel Burgr <pb@drake.cz>
2019-08-14 00:35:24 +0200

    WIP

--- fw/app.cpp
+++ fw/app.cpp
@@ -12,6 +12,44 @@ public:
 	}
 };
 
+void safeBootInit(int bootPin, bool logic, int ledPin = -1)
+{
+	target::PM.APBBMASK.setPORT(1);
+
+	target::PORT.DIRCLR.setDIRCLR(1 << bootPin);
+	if (logic)
+	{
+		target::PORT.OUTCLR.setOUTCLR(1 << bootPin);
+	}
+	else
+	{
+		target::PORT.OUTSET.setOUTSET(1 << bootPin);
+	}
+	target::PORT.PINCFG[bootPin].setPULLEN(1);
+
+	if (ledPin != -1)
+	{
+		target::PORT.DIRSET.setDIRSET(1 << ledPin);
+		target::PORT.OUTSET.setOUTSET(1 << ledPin);
+	}
+
+	for (volatile int w = 0; w < 20000; w++)
+	{
+		if (((target::PORT.IN.getIN() >> bootPin) & 1) == logic)
+		{
+			for (;;)
+				;
+		}
+	}
+
+	if (ledPin != -1)
+	{
+		target::PORT.OUTCLR.setOUTCLR(1 << ledPin);
+	}
+}
+
+/////////////////////////////////////////////////////////////////////
+
 class App
 {
 public:
@@ -20,9 +58,12 @@ public:
 	public:
 		void setTimerInterrupt(bool enabled)
 		{
-			if (enabled) {
+			if (enabled)
+			{
 				target::NVIC.ISER.setSETENA(11 << target::interrupts::External::TC1);
-			} else {
+			}
+			else
+			{
 				target::NVIC.ICER.setCLRENA(11 << target::interrupts::External::TC1);
 			}
 		}
@@ -53,17 +94,24 @@ public:
 
 	void init()
 	{
+		safeBootInit(25, false, LED_PIN);
+
+		for (;;)
+			;
+
 		timer.bind(this);
 
 		target::PM.APBBMASK.setPORT(1);
 		target::PORT.DIRSET.setDIRSET(1 << LED_PIN);
-		
-		
+
 		target::PORT.DIRSET.setDIRSET(1 << RF_DATA_PIN);
-		
+
 		target::PM.APBCMASK.setTC(1, 1);
-		target::GCLK.CLKCTRL = 1 << 14 | 0x12; // TC1 & TC2 clock from generator 0
-	
+		// target::GCLK.GENCTRL = 1 << 16 | 1 << 19 | 1;
+		// target::GCLK.CLKCTRL = 2 << 14 | 0x12; // TC1 & TC2 clock from generator 1
+
+		//target::GCLK.CLKCTRL = 1 << 14 | 0x12; // TC1 & TC2 clock from generator 0
+
 		target::TC1.COUNT16.CC[0].setCC(1000);
 		target::TC1.COUNT16.CTRLA.setWAVEGEN(1); // top = CC0
 		target::TC1.COUNT16.CTRLA.setENABLE(1);
@@ -71,13 +119,12 @@ public:
 
 		encoder.init(1);
 		timer.start(10);
-	
 	}
 
 } app;
 
 void interruptHandlerTC1()
-{	
+{
 	target::TC1.COUNT16.INTFLAG.setOVF(1);
 	app.encoder.handleTimerInterrupt();
 }
